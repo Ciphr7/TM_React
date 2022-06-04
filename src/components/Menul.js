c6727c7cecb1b4c0a7abae0c422e1940f3286e8e
Ciphr7 <gil@ciphr7.com>
2022-06-04 17:37:06 -0500

    updated menu

--- src/components/Menul.js
+++ src/components/Menul.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 
-import * as BsIcons from 'react-icons/bs'
+// import * as BsIcons from 'react-icons/bs'
 import { Link } from 'react-router-dom';
 import tmLogo from '../images/tmLogo.png'
 import vTruck from '../images/truck.png'
@@ -14,10 +14,52 @@ import MySelect from './RouteOptions'
 
 
 function Navbar() {
+
+  const [checked, setChecked] = useState(false); 
   const [sidebar, setSidebar] = useState(false);
-
+const loc1 =""
   const showSidebar = () => setSidebar(!sidebar);
-
+  const handleChange = () => {
+    setChecked(!checked);
+    console.log('The checkbox was toggled');
+    setOriginToCurrentLocation()
+  };
+  function componentDidMount() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      const loc1 = (lat + ':' + lng);
+    });
+  }
+  function setOriginToCurrentLocation() {
+    componentDidMount()
+    const b = this.checkbox === checked;
+    var self = this;
+    if (!b) {
+      this.input1 = "";
+    } else if (navigator.geolocation) {
+      var options = {
+        maximumAge: 0,
+        timeout: 30000,
+        enableHighAccuracy: true,
+      };
+      navigator.geolocation.getCurrentPosition(success, error, [options]);
+    } else {
+      alert("User did not allow access to GPS location");
+    }
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    function success(position) {
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
+      self.input1 = lat + ":" + lon;
+    }
+    /* When document is loaded fully...
+     ****************************************/
+  }
 
   return (
     <>
@@ -53,15 +95,18 @@ function Navbar() {
 
                   <label class="block">
                     <div className='flex bg-red-600 w-60 rounded-sm m-1 p-1'>
-                      <input type="checkbox" class=" checked:bg-blue-500 w-10 h-5 " />
+                      <input type="checkbox" onChange={handleChange} class=" checked:bg-blue-500 w-10 h-5 " 
+                       checked={checked} value={checked}/>
                       <span class="block text-sm font-sm text-white">Start at my GPS Location</span>
                     </div></label>
                   <div className='mx-8'>
+
                     <input className='searchBox p-1 m-2'
 
                       type="text"
                       placeholder="Search for Location"
-
+                      name = "loc1"
+                      value={loc1}
                     />
                     <input
                       className='searchBox p-1 m-2'
@@ -104,5 +149,6 @@ function Navbar() {
     </>
   );
 }
+
 
 export default Navbar;
