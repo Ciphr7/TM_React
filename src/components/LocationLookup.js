cc4a8d14bf5a093739822d9d043235cad375a0c8
ciphr7 <gil@ciphr7.com>
2023-11-20 12:34:42 -0600

    added location lookup page

--- src/components/LocationLookup.js
+++ src/components/LocationLookup.js
@@ -1,9 +1,115 @@
-import React from 'react'
+import React, { Component } from 'react';
+import MySelect from './RouteOptions'
 
-const LocationLookup = () => {
-  return (
-    <div>LocationLookup</div>
-  )
+import vTruck from '../images/truck.png'
+class LocationLookup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChecked: false,
+      locationValue: null,
+    };
+  }
+
+  // Function to get the user's current location
+  getGeolocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // Extract latitude and longitude from the position object
+          const { latitude, longitude } = position.coords;
+          this.setState({
+            locationValue: `${latitude}:${longitude}`,
+          });
+        },
+        (error) => {
+          console.error('Error getting geolocation:', error);
+          this.setState({ locationValue: null });
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported in this browser.');
+      this.setState({ locationValue: null });
+    }
+  };
+
+  // Function to handle checkbox change
+  handleCheckboxChange = () => {
+    const { isChecked } = this.state;
+    this.setState({ isChecked: !isChecked });
+
+    // Set the input value based on the checkbox state
+    if (isChecked) {
+      this.setState({ locationValue: null });
+    } else {
+      this.getGeolocation();
+    }
+  };
+
+  // Function to handle input value change
+  handleInputChange = (event) => {
+    this.setState({ locationValue: event.target.value });
+  };
+
+  render() {
+    const { isChecked, locationValue } = this.state;
+
+    return (
+      <>
+        <label className="block">
+        <div className='flex bg-red-600 w-60 rounded-sm m-1 p-1'>
+         
+          <input
+          className=" checked:bg-blue-500 w-10 h-5 "
+            type="checkbox"
+            checked={isChecked}
+            onChange={this.handleCheckboxChange}
+          />
+          <span className="block text-sm font-sm text-white">Start at my GPS Location</span>
+        </div></label>
+
+        <br />
+
+        <label>
+          <input className='searchBox p-1 m-2'
+            type="text"
+            value={locationValue === null ? '' : locationValue}
+            onChange={this.handleInputChange}
+            placeholder="Search for Location"
+          />
+          </label>
+        <input
+          className='searchBox p-1 m-2'
+          type="text"
+          onChange={this.handleInputChange}
+          placeholder="Search for Location"
+
+        />
+        <button className='bg-red-600 text-white mx-4 w-60 rounded-sm '>Run Trip</button>
+
+        <div><span className="block text-sm font-sm text-white">Trip Options</span></div> 
+        <MySelect />
+
+
+<div className='flex bg-red-600 w-60 rounded-sm m-1 p-1'>
+  <input type="checkbox" className=" checked:bg-blue-500 w-10 h-5 " />
+  <span className="block text-sm font-sm text-white w-40">Close Borders</span>
+</div>
+
+
+<div className='flex bg-red-600 rounded-sm w-60 m-1 p-1'>
+  <input type="checkbox" className=" checked:bg-blue-500 w-10 h-5 " />
+  <span className="block text-sm font-sm text-white ">Avoid Toll</span>
+</div>
+
+<img
+  className=' w-54 h-24 pt-3 m-2'
+  alt='TurckMiles Logo'
+  src={vTruck}
+/>
+      </>
+    );
+  }
 }
 
 export default LocationLookup
--- src/components/Menul.js
+++ src/components/Menul.js
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
-
-// import * as BsIcons from 'react-icons/bs'
-import { Link } from 'react-router-dom';
 import tmLogo from '../images/tmLogo.png'
-import vTruck from '../images/truck.png'
-
+// import * as BsIcons from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 import './Menul.css';
 import { IconContext } from 'react-icons';
-import MySelect from './RouteOptions'
-
+import LocationLookup from './LocationLookup';
 
 
 function Navbar() {
@@ -19,42 +15,8 @@ function Navbar() {
 
   const showSidebar = () => setSidebar(!sidebar);
   // State variables to manage checkbox and input values
-  const [isChecked, setIsChecked] = useState(false);
-  const [locationValue, setLocationValue] = useState(null);
 
-  // Function to get the user's current location
-  const getGeolocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          // Extract latitude and longitude from the position object
-          const { latitude, longitude } = position.coords;
-          setLocationValue(`Latitude: ${latitude}, Longitude: ${longitude}`);
-        },
-        (error) => {
-          console.error('Error getting geolocation:', error);
-          setLocationValue(null);
-        }
-      );
-    } else {
-      console.error('Geolocation is not supported in this browser.');
-      setLocationValue(null);
-    }
-  };
-
-  // Function to handle checkbox change
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked); // Toggle the checkbox value
-
-    // Set the input value based on the checkbox state
-    if (isChecked) {
-      setLocationValue(null);
-    } else {
-      getGeolocation();
-    }
-  }
-
-  return (
+ return (
     <>
 
       <IconContext.Provider value={{ color: '#fff' }}>
@@ -84,56 +46,7 @@ function Navbar() {
                     alt='TurckMiles Logo'
                     src={tmLogo}
                   />
-
-
-                  <label className="block">
-                    <div className='flex bg-red-600 w-60 rounded-sm m-1 p-1'>
-                      <input type="checkbox" checked={isChecked}
-                        onChange={handleCheckboxChange} className=" checked:bg-blue-500 w-10 h-5 "
-                      />
-                      <span className="block text-sm font-sm text-white">Start at my GPS Location</span>
-                    </div></label>
-                  <div className='mx-8'>
-
-                    <input className='searchBox p-1 m-2'
-
-                      type="text"
-                      value={locationValue === null ? '' : locationValue}
-                      placeholder="Search for Location"
-
-
-                    />
-                    <input
-                      className='searchBox p-1 m-2'
-                      type="text"
-                      placeholder="Search for Location"
-
-                    /></div>
-
-                  <button className='bg-red-600 text-white mx-4 w-60 rounded-sm '>Run Trip</button>
-
-                  <div><span className="block text-sm font-sm text-white">Trip Options</span></div>
-
-
-                  <MySelect />
-
-
-                  <div className='flex bg-red-600 w-60 rounded-sm m-1 p-1'>
-                    <input type="checkbox" className=" checked:bg-blue-500 w-10 h-5 " />
-                    <span className="block text-sm font-sm text-white w-40">Close Borders</span>
-                  </div>
-
-
-                  <div className='flex bg-red-600 rounded-sm w-60 m-1 p-1'>
-                    <input type="checkbox" className=" checked:bg-blue-500 w-10 h-5 " />
-                    <span className="block text-sm font-sm text-white ">Avoid Toll</span>
-                  </div>
-
-                  <img
-                    className=' w-54 h-24 pt-3 m-2'
-                    alt='TurckMiles Logo'
-                    src={vTruck}
-                  />
+                    <LocationLookup />
 
                 </div></form>
             </div>
